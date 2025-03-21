f96dbf7ba00d99de03bd1b34b77b8ceb0e2abcd6
Justin <meimar@ualberta.ca>
2025-03-21 15:47:57 -0600

    add web dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
 use yew::prelude::*;
+use web_sys::console;
+use serde::{Serialize, Deserialize};
 
 #[derive(Properties, PartialEq)]
 pub struct TextBoxProps {
     name: AttrValue,
     placeholder: AttrValue,
     #[prop_or_default] 
-    readonly: bool, 
+    readonly: bool,  
 }
 
 #[function_component]
-fn TextBox(props: &TextBoxProps) -> Html {
+fn TextBox(props: &TextBoxProps) -> Html { 
     html! {
         <textarea
             name={props.name.clone()}
             placeholder={props.placeholder.clone()}
             readonly={props.readonly}
-        >
-        </textarea>
+        />
     }
 }
 
@@ -28,9 +29,7 @@ pub struct SelectorProps {
 #[function_component]
 fn Selector(props: &SelectorProps) -> Html {
     html! {
-        <select
-            class={props.class.clone()}
-        >
+        <select class={props.class.clone()}>
             <option value="1"> {"1"} </option>
             <option value="2"> {"2"} </option>
             <option value="3"> {"3"} </option>
@@ -38,13 +37,38 @@ fn Selector(props: &SelectorProps) -> Html {
     }
 }
 
+#[derive(Properties, PartialEq)]
+pub struct TextEditorProps {
+    #[prop_or_default]
+    pub value: String,
+    #[prop_or_default]
+    pub onchange: Callback<String>,
+}
+
 #[function_component]
-fn TextEditor() -> Html {
+fn TextEditor(props: &TextEditorProps) -> Html {
+    let onchange = {
+        let callback = props.onchange.clone();
+        Callback::from(move |e: Event| {
+            let input = e.target_dyn_into::<web_sys::HtmlTextAreaElement>();
+            if let Some(input) = input {
+                console::log_1(&input.value().into());
+                console::log_1(&"Hello from Yew".into());
+                callback.emit(input.value());
+            }
+        })
+    };
+
     html! {
         <div>
             { "This is the code editor" }
-            <textarea width={"300"} height={"500"} rows={"24"}>
-            </textarea>
+            <textarea 
+                width={"300"} 
+                height={"500"} 
+                rows={"24"}
+                value={props.value.clone()}
+                onchange={onchange}
+            />
         </div>
     }
 }
@@ -59,8 +83,33 @@ fn CompilerOptions() -> Html {
     }
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(rename_all = "lowercase")]
+pub enum Compilers {
+    Generator,
+    SCalc,
+    VCalc,
+    Gazprea,
+}
+
+impl Compilers {
+    fn default() -> Self {
+        Compilers::Generator
+    }
+}
+
 #[function_component]
 fn App() -> Html {
+    
+    // TODO: Move into struct "AppState" ?
+    let code = use_state(|| String::new());
+    let compiler_option = use_state(|| Compilers::default());
+    let stdin = use_state(|| String::new());
+    let stdout = use_state(|| String::new());
+    let stderr = use_state(|| String::new());
+    
+    code.set("me".to_string());
+
     html! {
         <div>
             <div id="main-contianer">
@@ -68,6 +117,7 @@ fn App() -> Html {
                     <div id="editor" class="container">
                         <CompilerOptions/> 
                         <TextEditor/> 
+                        <button action="submit">{"Run"}</button>
                     </div>
                 </div>
                 <div id="right">
